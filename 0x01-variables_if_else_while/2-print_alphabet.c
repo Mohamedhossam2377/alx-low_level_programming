416735fa906c2520bd9c055b16508dd365398fda
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-16 10:04:38 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : alphabet game
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x = 'a';
+
+	for (x; x <= 'z'; x++)
+
+		putchar(x);
+
+	putchar('\n');
+	return (0);
+}
