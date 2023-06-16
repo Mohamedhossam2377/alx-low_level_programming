90c357b9d1f995e3f88d12ca78fc2e4125e3461e
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-16 16:26:40 -0700

    two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,23 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 99; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b <= 99; b++)
 		{
-			for (c = 0; c <= 9; c++)
+			if (a != b)
 			{
-				for (d = 0; d <= 9; d++)
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+				putchar((b % 10) + '0');
+				if (a != 98 || b != 99)
 				{
-					if (a != c || b != d || a + b < c + d)
-					{
-					if (a + b <= c + d || (b == c && a == d))
-					{
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(' ');
-						putchar(c + '0');
-						putchar(d + '0');
-					if (a + b != 17 || c + d != 18)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					}
-					}
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
