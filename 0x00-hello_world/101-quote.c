35b18efce652ccb601912de78b64f83713c3d292
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-15 18:19:28 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  * main - start point
  *
@@ -7,8 +7,7 @@
  * Return: 1 not success*
 */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	Return(1);
