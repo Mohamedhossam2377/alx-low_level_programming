7fd4fba1b5549520fb923589d74ff5e0ca43d849
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-16 12:47:43 -0700

    two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,8 +20,11 @@ int main(void)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
-				putchar(',');
-				putchar(' ');
+				if (a != 8 && b != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
