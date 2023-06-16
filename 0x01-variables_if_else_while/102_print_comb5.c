9c7f664d5ebe1024bbbba3063d7fcacd04fd08a6
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-16 15:41:49 -0700

    two two digits

---
+++ 0x01-variables_if_else_while/102_print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : two two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			for (c = 0; c <= 9; c++)
+			{
+				for (d = 0; d <= 9; d++)
+				{
+					if (a != c && b != d && a + b < c + d)
+					{
+						putchar(a + '0');
+						putchar(b + '0');
+						putchar(c + '0');
+						putchar(d + '0');
+					if (a + b != 17 || c + d != 18)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+}
