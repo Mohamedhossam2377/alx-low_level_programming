499ae75763342642fd4c80d19d0152c5a4df85fc
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 13:00:14 -0700

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,14 +8,16 @@
 void jack_bauer(void)
 {
 	int x, y;
+
 	for (x = 0; x <= 23; x++)
 	{
 		for (y = 0; y <= 60; y++)
 		{
-			_putchar(x / 10);
-			_putchar(x % 10);
-			_putchar(y / 10);
-			_putchar(y % 10);
+			_putchar((x / 10) + '0');
+			_putchar((x % 10) + '0');
+			_putchar(':');
+			_putchar((y / 10) + '0');
+			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
