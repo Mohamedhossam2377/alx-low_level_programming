135554efb867887a013b0f8413a2409c1a88a2b1
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 16:40:36 -0700

    fibonacci_even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Discreption :
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long f1 = 0, f2 = 1, result;
+	float total_result;
+
+	for (i = 1; result <= 4000000; i++)
+	{
+		result = f1 + f2;
+
+		if (result % 2 == 0)
+		{
+			total_result += result;
+		}
+		f1 = f2;
+		f2 = result;
+	}
+}
