f8235f3982d181f81985c4395b678803a21937d4
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 11:43:43 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,14 +15,14 @@ int print_sign(int n)
 		_putchar(43);
 		return (1);
 	}
-	else if (n == 0)
+	else if
 	{
 		_putchar(48);
 		return (0);
 	}
 	else
 	{
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 }
