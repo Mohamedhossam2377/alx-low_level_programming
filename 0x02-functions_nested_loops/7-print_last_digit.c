8a75575104c608b390c8bf0b44cd0c4d2d6771f3
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 12:36:10 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,14 @@
  */
 int print_last_digit(int n)
 {
-	int last;
-
 	if (n < 0)
 	{
-		last = (-1) * (n % 10);
+		n = (-1) * (n % 10);
 	}
 	else
 	{
-		last = n % 10;
+		n = n % 10;
 	}
-	_puchar(last + '0');
-	return (last);
+	_putchar(n + '0');
+	return (n);
 }
