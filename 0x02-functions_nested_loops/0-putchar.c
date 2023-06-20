cd736a35b56d9054884f517dd01853a0e72b5533
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 08:28:40 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description : print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char st[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(st[ch]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
