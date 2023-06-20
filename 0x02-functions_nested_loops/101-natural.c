23336ab8aea3db2459670f0794b2924b88c9a822
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 15:49:02 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Discreption: natural numbers bellow 1024
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			count = count + i;
+		}
+	}
+	printf("%d\n", count);
+	return (0);
+}
