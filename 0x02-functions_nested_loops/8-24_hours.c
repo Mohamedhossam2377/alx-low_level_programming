8f6fceefddfe3d864e62d7d05adeb34fda818340
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 12:52:24 -0700

    jack bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * jack_bauer - every minute
+ *
+ * Return: always return 0 (Success)
+ */
+void jack_bauer(void)
+{
+	for (int x; x <= 23; x++)
+	{
+		for (int y; y <= 60; y++)
+		{
+			_putchar(x / 10);
+			_putchar(x % 10);
+			_putchar(y / 10);
+			_putchar(y % 10);
+		}
+		_putchar('\n');
+	}
+}
