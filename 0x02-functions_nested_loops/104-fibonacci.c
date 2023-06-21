f6510fb7e5225c5e2157512964ccc95ab3b83c8a
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-20 17:27:20 -0700

    fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -40,7 +40,7 @@ int main(void)
 			printf("%lu", f1o);
 		initial0s = numlength(mx) - 1 - numlength(f1);
 
-		while (f1 > 0 && initial0s > 0)
+		while (f1o > 0 && initial0s > 0)
 		{
 			printf("%d", 0);
 			initial0s--;
