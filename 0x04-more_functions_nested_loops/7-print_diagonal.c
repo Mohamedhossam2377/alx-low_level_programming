106a027c350dd189ade170f15330e17548c14605
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-22 15:38:32 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,6 +16,7 @@ void print_diagonal(int n)
 	{
 		if (n <= 0)
 		{
+			_putchar('\n');
 			break;
 		}
 
@@ -27,5 +28,4 @@ void print_diagonal(int n)
 		}
 		_putchar(' ');
 	}
-	_putchar('\n');
 }
