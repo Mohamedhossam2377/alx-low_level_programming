1fd0d53291c7d25d04db58ae9d42fd01189b9e9b
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-22 17:04:29 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,22 +12,32 @@ int main(void)
 
 	for (i = 1; i < 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && !(i % 5 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && !(i % 3 == 0))
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
+
+		if (i == 100)
+		{
+			printf("\n");
+		}
+		else
+		{
+			printf(" ");
+		}
 	}
-	printf("Buzz\n");
+
+	return (0);
 }
