41a624cc774caebce19a6d190f99b0c9d53b6222
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-26 13:59:36 -0700

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_rev - print string in reverse
+ *
+ * @s: string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
