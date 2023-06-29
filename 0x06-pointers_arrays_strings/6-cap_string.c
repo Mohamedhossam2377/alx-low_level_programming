6116794399ef356d359e991a5ae311005b84537f
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-06-29 09:11:49 -0700

    cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,60 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * is_lower - determines if is lowercase
+ *
+ * @c: character
+ *
+ * Return: 1 if ture and 0 if false
+ */
+int is_lower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * is_delimiter - determine if ascii is a delimiter
+ *
+ * @c: character
+ *
+ * Return: 1 if true and 0 if false
+ */
+int is_delimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalize all words
+ *
+ * @str: string to capitalize
+ *
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int found_delimiter = 1;
+
+	while (*str)
+	{
+		if (is_delimter(*str))
+			found_delimiter = 1;
+		else if (is_lower(*str) && found_delimiter)
+		{
+			*str -= 32;
+			found_delimiter = 0;
+		}
+		else
+			found_delimiter = 0;
+		str++;
+	}
+	return (ptr);
+}
