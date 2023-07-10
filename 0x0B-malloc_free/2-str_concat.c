e6ce5e0cbb2005ec6d9a0ed5a4cdec1586585af8
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-07-10 08:27:40 -0700

    str concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -32,9 +32,9 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	len1 = slength(s1);
 	len2 = slength(s2);
