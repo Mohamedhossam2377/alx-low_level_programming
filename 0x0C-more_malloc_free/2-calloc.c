1aec16c891c3090193928e465062254905512a59
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-07-13 09:04:46 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,26 +3,6 @@
 #include "main.h"
 
 /**
- * fill_memory - fill memory with constant
- *
- * @s: pointer
- * @c: constant
- * @num: max bytes to use
- *
- * Return: s
- */
-char *fill_memory(char *s, char c, unsigned int num)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		s[i] = c;
-	}
-
-	return (s);
-}
-/**
  * _calloc - allocates memory for an array, using malloc
  *
  * @nmemb: memory for an array
@@ -33,6 +13,7 @@ char *fill_memory(char *s, char c, unsigned int num)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
@@ -41,7 +22,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (p == NULL)
 		return (NULL);
 
-	fill_memory(p, 0, size * nmemb);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
 
 	return (p);
 }
