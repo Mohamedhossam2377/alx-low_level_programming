111d17c8b105661bdc3d5a28885d45670b099e0c
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-07-17 09:57:00 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ *
+ * Return: Always 0.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
