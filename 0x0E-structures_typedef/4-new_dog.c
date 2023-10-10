9bce9d286ae7f378f090cf7625aaa9452322f7e1
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-10 15:32:52 -0700

    new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,79 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * len - length of string
+ *
+ * @s: string
+ *
+ * Return: length
+ */
+int len(const char *s)
+{
+	int l = 0;
+
+	while (*s++)
+		l++;
+	return (l);
+}
+
+/**
+ * scopy - copy string to new_string
+ *
+ * @st: string
+ * @new_string: string to copy in
+ *
+ * Return: new_string
+ */
+char *scopy(char *new_string, char *st)
+{
+	int i;
+
+	for (i = 0; st[i]; i++)
+		new_string[i] = st[i];
+	new_string[i] = '\0';
+
+	return (new_string);
+}
+
+/**
+ * new_dog - creates a new dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of dog's owner
+ *
+ * Return: struct pointer dog or null if fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	if (!name || age < 0 || !owner)
+		return (NULL);
+
+	dog = (dog_t *) malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len(name) + 1));
+	if ((*dog).name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (len(owner) + 1));
+	if ((*dog).owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	dog->name = scopy(dog->name, name);
+	dog->age = age;
+	dog->owner = scopy(dog->owner, owner);
+
+	return (dog);
+}
