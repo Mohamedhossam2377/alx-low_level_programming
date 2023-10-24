d976e99e9fd6605c46ea9a009e562a1f4a167220
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-24 10:00:42 -0700

    print listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ *
+ * @h: first node
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	for (size_t sum = 0; h != null; sum++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (sum);
+}
