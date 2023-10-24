12311d6601f360a2b2b7a2a6fa21ee68630e2aa1
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-24 13:07:31 -0700

    insert nodeint

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,7 @@ include "lists.h"
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *node;
-	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *node, *new = malloc(sizeof(listint_t));
 	unsigned int i = 0;
 
 	if (head == NULL || new == NULL)
@@ -27,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 	node = *head;
-	while (node != NULL)
+	while (node)
 	{
 		if (i == idx - 1)
 		{
