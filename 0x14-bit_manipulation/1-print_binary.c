62c573f2eb0eb11c8384f250c13c879906aca137
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-26 16:53:18 -0700

    pri

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_binary - converts a binary number to an unsigned int
+ *
+ * @n: number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int size = sizeof(n) * 8, print = 0;
+}
