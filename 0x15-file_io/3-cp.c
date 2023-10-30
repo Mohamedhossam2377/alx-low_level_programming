5f192a300b4e857d50e638c52cf694d62a8a995e
mohamedhossam2377 <m.hossam2377@gmail.com>
2023-10-30 15:06:47 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,32 +16,32 @@
  */
 int main(int ac, char **av)
 {
-	int fp_from = 0;
-	int fp_to = 0;
+	int fd_from = 0;
+	int fd_to = 0;
 	ssize_t num_bytes;
 	char buffer[READ_BUF_SIZE];
 
 	if (ac != 3)
 		dprintf(STDERR_FILENO, USAGE), exit(97);
-	fp_from = open(av[1], O_RDONLY);
-	if (fp_from == -1)
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	fp_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-	if (fp_to == -1)
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
+	if (fd_to == -1)
 		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 
-	while ((num_bytes = read(fp_from, buffer, READ_BUF_SIZE)) > 0)
-		if (write(fp_to, buffer, num_bytes) != num_bytes)
+	while ((num_bytes = read(fd_from, buffer, READ_BUF_SIZE)) > 0)
+		if (write(fd_to, buffer, num_bytes) != num_bytes)
 			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	if (num_bytes == -1)
 		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
-	fp_from = close(fp_from);
-	fp_to = close(fp_to);
-	if (fp_from)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fp_from), exit(100);
-	if (fp_to)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, fp_from), exit(100);
+	fd_from = close(fd_from);
+	fd_to = close(fd_to);
+	if (fd_from)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
+	if (fd_to)
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
 
 	return (EXIT_SUCCESS);
 }
