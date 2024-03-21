2ebd78e462dfa80bd48a2ca0986f8b7ed989c05c
mohamedhossam2377 <m.hossam2377@gmail.com>
2024-03-21 08:29:31 -0700

    add dndeint

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add new head node at the beginning
+ *
+ * @head: current head node
+ * @n: new node field
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+	if (!head || !new_node)
+		return (new_node ? free(new_node), NULL : NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	if (!*head)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+		new_node->next = *head;
+		(*head)->prev = new_node;
+		*head = new_node;
+	}
+	return (new_node);
+}
